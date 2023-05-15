bc66a535a43ec33fa2dbdeb7bf9e9457f087f0cb
Ruan Narici <ruannarici@hotmail.com>
2023-05-15 20:08:20 -0300

    upload atividades

---
+++ tema6Exercicios/projeto1/main.c
@@ -0,0 +1,24 @@
+//Faça um programa que leia 100 números inteiros e
+//mostre-os na ordem inversa em que foram lidos.
+//Em relação à estrutura de dados, o vetor vai armazenar 100 números inteiros.
+
+#include <stdio.h>
+
+void main() {
+
+    int qtd = 10;
+    int numeros[qtd];
+
+    for (int i = 0; i < qtd; i++) {
+
+        printf("Digite um numero: ");
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("#-> Numero na ordem inversa \n");
+
+    for (int i = qtd -1; i >= 0; i--) {
+        printf("%d\n", numeros[i]);
+    }
+
+}
